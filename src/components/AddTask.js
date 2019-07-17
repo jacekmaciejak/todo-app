73fb89c18a07c4d8f7363981932d26ca14d3d603
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-07-17 14:57:09 +0200

    Components added

---
+++ src/components/AddTask.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class TaskList extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="form">
+        <div>Dodaj task</div>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default TaskList;
---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        todo app
+        <AddTask />
+        <TaskList />
+      </div>
+    );
+  }
+}
+
+export default App;
